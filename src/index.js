505aed024bf37a3e36e9652b74471754498b1b42
Alex Canessa <canessa.alex@gmail.com>
2018-07-11 00:16:49 +0100

    fix(src): remove last item from array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const test = (str = "", obj = {}) => console.log(`Log: ${str}`, object);
 const otherTest = (num = 0) => num + 1000;
-const lastTest = (arr = []) => arr.slice(0, 1);
+const lastTest = (arr = []) => arr.slice(0, -1);
 
 export default test;
 
