dee42978788d65e003443963c8543d219cf71e9b
Alex Canessa <canessa.alex@gmail.com>
2018-07-11 00:11:06 +0100

    fix(src): add another 0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const test = (str = "", obj = {}) => console.log(`Log: ${str}`, object);
-const otherTest = (num = 0) => num + 100;
+const otherTest = (num = 0) => num + 1000;
 
 export default test;
 
