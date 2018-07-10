4c456381654fdcb5f7365450e737738910d7e60a
Alex Canessa <canessa.alex@gmail.com>
2018-07-11 00:15:30 +0100

    feat(src): add another test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const test = (str = "", obj = {}) => console.log(`Log: ${str}`, object);
 const otherTest = (num = 0) => num + 1000;
+const lastTest = (arr = []) => arr.slice(0, 1);
 
 export default test;
 
 export {
-  otherTest
+  otherTest,
+  lastTest
 };
